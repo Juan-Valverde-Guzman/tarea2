b8e29a648f79ae94e61d066364e16b16df91c75e
dam2 <dam2@Mac-Mini-04.local>
2024-02-16 17:51:33 +0100

    El backend

---
+++ tarea2Back/src/main/java/com/example/tarea2Back/Tarea2BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarea2Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2BackApplication.class, args);
+	}
+
+}
---
+++ tarea2Back/src/test/java/com/example/tarea2Back/Tarea2BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarea2Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
